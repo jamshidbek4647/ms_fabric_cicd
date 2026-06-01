a5aadb2912624b1de754f8fd784d2af31615b563
Jamshidbek Orifjonov <145925265+jamshidbek4647@users.noreply.github.com>
2026-06-01 10:26:27 +0500

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -27,7 +27,7 @@ workspace = FabricWorkspace(
     workspace_id=workspace_id,
     environment=target_env,
     repository_directory=str(Path(__file__).resolve().parent),
-    item_type_in_scope=["Notebook", "DataPipeline", "Environment"],
+    item_type_in_scope=["all"],
     token_credential=credential,
 )
 
